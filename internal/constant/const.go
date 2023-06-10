424a3d4ac3072f1bebe47f6feeed43160bb9d071
mdanialr <mdanialrma@gmail.com>
2023-06-10 19:12:48 +0700

    feat: constant for domains

---
+++ internal/constant/const.go
@@ -0,0 +1,7 @@
+package cons
+
+const (
+	UsedOTP    = "USED_OTP"
+	InvalidOTP = "INVALID_OTP"
+	DepsErr    = "DEPS_ERROR"
+)
---
+++ internal/constant/error.go
@@ -0,0 +1,10 @@
+package cons
+
+import "errors"
+
+var (
+	ErrUsedOTP        = errors.New("otp has been used")
+	ErrInvalidOTP     = errors.New("invalid otp")
+	ErrInternalServer = errors.New("something wasn't right")
+	ErrSigningToken   = errors.New("signing token error")
+)
