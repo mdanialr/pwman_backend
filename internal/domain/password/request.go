61cc23f6f526f903896d1273499301aef18f70d1
mdanialr <mdanialrma@gmail.com>
2023-06-18 18:09:16 +0700

    ref: DRY - reuse pagination and separate it in different file

--- internal/domain/password/request.go
+++ internal/domain/password/request.go
@@ -4,48 +4,17 @@ import (
 	"mime/multipart"
 	"strings"
 
-	paginate "github.com/mdanialr/pwman_backend/pkg/pagination"
-
 	"github.com/go-playground/validator/v10"
 )
 
 // Request standard request object that may be used in password domain.
 type Request struct {
-	paginate.M
-	// Order the field name to query Order. Default to id.
-	Order string `json:"-" query:"order"`
-	// Sort to query Order. Should be filled with either asc or desc. Default
-	// to asc.
-	Sort string `json:"-" query:"sort"`
-	// Search do search for category name and or username from given string.
-	Search string `json:"-" query:"search"`
-}
-
-// SetQuery do setup Order and Sort.
-func (r *Request) SetQuery() {
-	if r.Order == "" {
-		r.Order = "id" // set default to id
-	}
-	// sanitize Sort
-	r.Sort = r.sanitizeQuerySort()
-	if r.Sort == "" {
-		r.Sort = "asc" // set default to asc
-	}
-	// make sure the Sort is upper-cased
-	r.Sort = strings.ToUpper(r.Sort)
-}
-
-// sanitizeQuerySort make sure Sort has the expected value.
-func (r *Request) sanitizeQuerySort() string {
-	switch strings.ToLower(r.Sort) {
-	case "asc", "desc":
-		return r.Sort
-	}
-	return ""
+	pagination
 }
 
 // RequestCategory standard request object that may be used in password domain.
 type RequestCategory struct {
+	pagination
 	// ID unique identifier of each Category. Should be required when updating.
 	ID uint `form:"id"`
 	// Name the name of category.
@@ -56,37 +25,6 @@ type RequestCategory struct {
 	// Icon binary file for icon field that should be parsed manually from
 	// delivery.
 	Icon *multipart.FileHeader `form:"icon"`
-	paginate.M
-	// Order the field name to query Order. Default to id.
-	Order string `json:"-" query:"order"`
-	// Sort to query Order. Should be filled with either asc or desc. Default
-	// to asc.
-	Sort string `json:"-" query:"sort"`
-	// Search do search for category name from given string.
-	Search string `json:"-" query:"search"`
-}
-
-// SetQuery do setup Order and Sort.
-func (r *RequestCategory) SetQuery() {
-	if r.Order == "" {
-		r.Order = "id" // set default to id
-	}
-	// sanitize Sort
-	r.Sort = r.sanitizeQuerySort()
-	if r.Sort == "" {
-		r.Sort = "asc" // set default to asc
-	}
-	// make sure the Sort is upper-cased
-	r.Sort = strings.ToUpper(r.Sort)
-}
-
-// sanitizeQuerySort make sure Sort has the expected value.
-func (r *RequestCategory) sanitizeQuerySort() string {
-	switch strings.ToLower(r.Sort) {
-	case "asc", "desc":
-		return r.Sort
-	}
-	return ""
 }
 
 // Validate apply validation rules for RequestCategory.
